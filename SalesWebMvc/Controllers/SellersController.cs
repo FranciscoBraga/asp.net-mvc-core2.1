96cd8e1703d9607311b95c4361079a33c2bac88c
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-10 17:40:24 -0200

    ViewModel para auxiliar selectlist

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Service;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartamentService _departamentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartamentService departamentService)
         {
             _sellerService = sellerService;
+            _departamentService = departamentService;
         }
 
         public IActionResult Index()
@@ -25,14 +28,17 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departaments = _departamentService.FindAll();
+            var viewModel = new SellerFormViewModelcs { Departaments = departaments };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller sellers)
+        public IActionResult Create(SellerFormViewModelcs sellers)
         {
-            _sellerService.Insert(sellers);
+            Seller s = sellers.Seller;           
+            _sellerService.Insert(s);
             return RedirectToAction(nameof(Index));
         }
     }
