18204d1049541d718ad090d908d3689b73192c2c
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-10 10:21:29 -0200

    Tela simples de cadastro

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Service;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var lista = _sellerService.FindAll();
             return View(lista);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller sellers)
+        {
+            _sellerService.Insert(sellers);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
