7283f1aa3fb2823b9ae2fec1731e05b1d73f2da8
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-15 17:52:26 -0200

    Direcionamento a página de erro -quando o deleta vendedor com vendas

--- SalesWebMvc/Service/SellerService.cs
+++ SalesWebMvc/Service/SellerService.cs
@@ -37,9 +37,17 @@ namespace SalesWebMvc.Service
 
         public async Task RemoveAsync(int id)
         {
-            var obj =  await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("can't delete seller he/she has sales");
+            }
+         
 
         }
 
