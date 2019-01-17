b83fc38ec06aa7b51cd7067f79c62ff22136403d
Francisco Oliveira Braga <associacaogames@gmail.com>
2019-01-17 13:41:16 -0200

    implementando busca em grupo

--- SalesWebMvc/Service/SalesRecordService.cs
+++ SalesWebMvc/Service/SalesRecordService.cs
@@ -16,7 +16,7 @@ namespace SalesWebMvc.Service
             _context = context;
         }
 
-        public async Task<List<SalesRecord>> FindByDate(DateTime? minDate, DateTime? maxDate)
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
             var result = from obj in _context.SalesRecord select obj;
             if (minDate.HasValue)
@@ -31,6 +31,28 @@ namespace SalesWebMvc.Service
                     .Include(x => x.Seller)
                     .Include(x => x.Seller.Departament)
                     .OrderByDescending(x => x.Date)
+                    .ToListAsync();
+
+
+        }
+
+
+        public async Task<List<IGrouping<Departament,SalesRecord>>> FindByDateGroupAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                    .Include(x => x.Seller)
+                    .Include(x => x.Seller.Departament)
+                    .OrderByDescending(x => x.Date)
+                    .GroupBy(x =>x.Seller.Departament)
                     .ToListAsync();
 
 
